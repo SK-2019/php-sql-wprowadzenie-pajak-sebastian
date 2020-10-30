b13ce3b14cb99d556dfbdd839ff4b4de6325d720
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-10-30 14:21:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -31,4 +31,20 @@ $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
             }
         echo("</table>");
 
+
+echo("<h2>Zadanie 2</h2>");
+echo("<h3>SELECT * FROM pracownicy where (dzial=1 or dzial=2)</h3>");
+$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query('SELECT * FROM pracownicy where (dzial=1 or dzial=2)');
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
 ?>
