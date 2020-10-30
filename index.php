28961b6956a603d2ffaf27c2400c8573dfa2babf
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-10-30 11:35:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
-
+ $result=$conn->query('SELECT * FROM pracownicy');
+        echo("<table border=1>");
+        echo("<th>id</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
 echo("<h1>Sebastian Pająk</h1>");
 
 ?>
