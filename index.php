d1c1701b9bd16a9d152d9c7a9b50b00e75fe6753
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-03 09:50:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
 echo("<h2>Zadanie 1</h2>");
 echo("<h3>SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and  (dzial = 1)</h3>");
 $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query('SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM "pracownicy", "organizacja" WHERE (dzial = id_org) and  (dzial = 1)');
+ $result=$conn->query('SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and  (dzial = 1)');
         echo("<table border=1>");
         echo("<th>imie</th>");
         echo("<th>zarobki</th>");
@@ -39,65 +39,65 @@ $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
 
 
 echo("<h2>Zadanie 2</h2>");
-echo("<h3>SELECT * FROM pracownicy where (dzial=1 or dzial=2)</h3>");
+echo("<h3>SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (dzial=1 or dzial=2)</h3>");
 $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query('SELECT * FROM pracownicy where (dzial=1 or dzial=2)');
+ $result=$conn->query('SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (dzial=1 or dzial=2)');
         echo("<table border=1>");
-        echo("<th>id</th>");
         echo("<th>imie</th>");
-        echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
             while($row=$result->fetch_assoc()) {
                 echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
                 echo("</tr>");
             }
         echo("</table>");
 
 echo("<h2>Zadanie 3</h2>");
-echo("<h3>SELECT * FROM pracownicy where (dzial=1 or dzial=2) and (imie like '%a')</h3>");
+echo("<h3>SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (dzial=1 or dzial=2) and (imie like '%a')</h3>");
 $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query('SELECT * FROM pracownicy where (dzial=1 or dzial=2) and (imie like "%a")');
+ $result=$conn->query('SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (dzial=1 or dzial=2) and (imie like '%a')');
         echo("<table border=1>");
-        echo("<th>id</th>");
         echo("<th>imie</th>");
-        echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
             while($row=$result->fetch_assoc()) {
                 echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
                 echo("</tr>");
             }
         echo("</table>");
 
 echo("<h2>Zadanie 4</h2>");
-echo("<h3>SELECT * FROM pracownicy where imie like '%a'</h3>");
+echo("<h3>SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (imie like '%a')</h3>");
 $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query('SELECT * FROM pracownicy where imie like "%a"');
+ $result=$conn->query('SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (imie like '%a')');
         echo("<table border=1>");
-        echo("<th>id</th>");
         echo("<th>imie</th>");
-        echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
             while($row=$result->fetch_assoc()) {
                 echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
                 echo("</tr>");
             }
         echo("</table>");
 
 echo("<h2>Zadanie 5</h2>");
-echo("<h3>SELECT * FROM pracownicy where zarobki>50</h3>");
+echo("<h3>SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (zarobki>50)</h3>");
 $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query('SELECT * FROM pracownicy where zarobki>50');
+ $result=$conn->query('SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (zarobki>50)');
         echo("<table border=1>");
-        echo("<th>id</th>");
         echo("<th>imie</th>");
-        echo("<th>dzial</th>");
         echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
             while($row=$result->fetch_assoc()) {
                 echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
                 echo("</tr>");
             }
         echo("</table>");
