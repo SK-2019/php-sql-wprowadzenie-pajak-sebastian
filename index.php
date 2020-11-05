00942ff17a6c5a201aad115bd228bbd2068385a8
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-05 11:25:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
    <div class="nav"> 
     <h2>Linki</h2>
     <h4><a href="https://github.com/SK-2019/php-sql-wprowadzenie-pajak-sebastian"><b>GITHUB</b></a></h4>
-    <a href="orgPracownicy.php">Org i Prac</a>
-    <a href="agregat.php">Funkcje agregujące</a>
+    <a href="orgPracownicy.php">ORGANIZACJA I PRACOWNICY</a>
+    <a href="agregat.php">FUNKCJE AGREGUJĄCE</a>
 </div>    
  <?php
  
