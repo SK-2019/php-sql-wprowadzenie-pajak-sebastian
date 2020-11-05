7fae62575a02da772829b9edf15206e0cc942aeb
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-05 10:09:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -164,6 +164,7 @@ $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
 
 ?>
 <a href="#"><img class="obraz" src="yma.png"></a>
+ <a href="#"><img class="obraz2" src="yma.png"></a>
  
 
 
