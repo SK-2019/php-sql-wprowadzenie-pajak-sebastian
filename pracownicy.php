f587efa0d3b77008bd58bd5aa2de9039f455e86e
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-06 10:55:11 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -40,6 +40,24 @@ $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
                 echo("</tr>");
             }
         echo("</table>");
+  
+  echo("<h1>Sebastian Pająk</h1>");
+echo("<h2>Pracownicy tylko z zarobkami mniejszymi niz 30</h2>");
+$sql = "SELECT * FROM pracownicy, organizacja where (dzial = id_org) and (zarobki<30)";
+$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+  require("conn.php");
+        echo("<table border=1>");
+        echo("<th>imie</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
   ?>
   
   
