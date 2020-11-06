d2bf2b81da9771a2323f7d471f3e9e680e22e6b6
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-06 11:07:52 +0100

    Update agregat.php

--- agregat.php
+++ agregat.php
@@ -18,4 +18,34 @@ $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
                 echo("</tr>");
             }
         echo("</table>");
+ 
+ echo("<h2>Zadanie 2</h2>");
+ $sql = "SELECT sum(zarobki) as sum FROM pracownicy, organizacja WHERE (dzial = id_org) and (imie like 'a%')";
+echo("<h3>".$sql."</h3>");
+$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+        echo("<table border=1>");
+     
+        echo("<th>sum</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["sum"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+ 
+ echo("<h2>Zadanie 3</h2>");
+ $sql = "SELECT sum(zarobki) as sum FROM pracownicy, organizacja WHERE (dzial = id_org and imie not like 'a%') and (dzial = 2 or dzial = 3)";
+echo("<h3>".$sql."</h3>");
+$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+        echo("<table border=1>");
+     
+        echo("<th>sum</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["sum"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
 ?>
