02771a32f9de95a05ae5527646bd449086153437
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-06 15:28:52 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -12,6 +12,8 @@
     <a href="agregat.php">FUNKCJE AGREGUJĄCE</a>
       <h6>   </h6>
     <a href="pracownicy.php">PRACOWNICY</a>
+    <h6>   </h6>
+  <a href="dataiczas.php">DATA I CZAS</a>
 </div>    
   
   <?php
