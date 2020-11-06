2ebb26abb343f3b8fe56cc7786f38545f5c61d5a
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-06 11:06:12 +0100

    Update agregat.php

--- agregat.php
+++ agregat.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
 <?php
-echo("agregat.php")
+echo("<h2>Zadanie 1</h2>");
+ $sql = "SELECT sum(zarobki) as sum FROM pracownicy, organizacja WHERE (dzial = id_org)";
+echo("<h3>".$sql."</h3>");
+$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+        echo("<table border=1>");
+     
+        echo("<th>sum</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["sum"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
 ?>
