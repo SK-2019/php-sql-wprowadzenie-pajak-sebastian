2fc6673fa446efb19c443fbc7cbfec4990228251
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-10 10:00:52 +0100

    Update dataiczas.php

--- dataiczas.php
+++ dataiczas.php
@@ -23,10 +23,11 @@ $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
  $result=$conn->query($sql);
         echo("<table border=1>");
         echo("<th>nazwa_dzial</th>");
+        echo("<th>imie</th>");
         echo("<th>wiek</th>");
             while($row=$result->fetch_assoc()) {
                 echo("<tr>");
-                    echo("<td>".$row["nazwa_dzial"]."</td><td>".$row["wiek"]."</td>");
+                    echo("<td>".$row["nazwa_dzial"]."</td><td>".$row["imie"]."</td><td>".$row["wiek"]."</td>");
                 echo("</tr>");
             }
         echo("</table>");
