294c4720e5ad4b2f646403aa30e06b6802f3381a
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-10 09:59:23 +0100

    Update dataiczas.php

--- dataiczas.php
+++ dataiczas.php
@@ -17,7 +17,7 @@
  <?php
 
 echo("<h2>Wiek poszczególnych pracowników (w latach)</h2>");
- $sql = "SELECT * ,YEAR(curdate())-YEAR(data_urodzenia) AS wiek FROM pracownicy";
+ $sql = "SELECT * ,YEAR(curdate())-YEAR(data_urodzenia) AS wiek FROM pracownicy, organizacja WHERE (dzial = id_org)";
 echo("<h3>".$sql."</h3>");
 $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
  $result=$conn->query($sql);
