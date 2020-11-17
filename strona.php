41e3797d6fdffca95a544d65a9cf8a5a92fe4a18
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-17 10:02:48 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -6,10 +6,14 @@
 
 <?php
 echo ("strona.php");
+  <h1> | </h1>
 
 echo($_POST['firstname']);
+   <h1> | </h1>
 echo($_POST['lastname']);
+   <h1> | </h1>
 echo($_POST['city']);
+   <h1> | </h1>
 echo($_POST['phone']);
 
 
