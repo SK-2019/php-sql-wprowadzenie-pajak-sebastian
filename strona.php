29de9c9bb7d2f81f4044278126f07b30d93401f7
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-19 10:53:25 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -5,11 +5,12 @@
 </head>
 
 <?php
-  
+ echo("jesteś na stronie.php");
+  echo("<ul>");
 echo("<li> imie: ".$_POST['firstname']."</li>");
 echo("<li> nazwisko: ".$_POST['lastname']."</li>");
 echo("<li> miasto: ".$_POST['city']."</li>");
 echo("<li> telefon: ".$_POST['phone']."</li>");
-
+  echo("<ul>");
 
 ?>
