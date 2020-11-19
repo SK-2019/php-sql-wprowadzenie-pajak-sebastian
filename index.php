0ea7f82e88582ce9772126a4b9cdf0d0b85baff4
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-19 21:45:32 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
   <title>strona glowna sebola</title>
 </head>
 <?php
-echo("<h1>Sebastian Pająk</h1>");
+echo("<h3>Sebastian Pająk</h3>");
 ?>
    <div class="nav"> 
-    </br>
+    
    
     <h4><a href="https://github.com/SK-2019/php-sql-wprowadzenie-pajak-sebastian"><b>GITHUB</b></a></h4></br>
       
@@ -54,164 +54,6 @@ $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
                 echo("</tr>");
             }
         echo("</table>");
-
-echo("<h2>Zadanie 1</h2>");
-$sql = "SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and  (dzial = 1)";
-echo("<h3>".$sql."</h3>");
-$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query($sql);
- require("conn.php");
-        echo("<table border=1>");
-        echo("<th>imie</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>data_urodzenia</th>");
-        echo("<th>nazwa_dzial</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-
-
-echo("<h2>Zadanie 2</h2>");
-$sql = "SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (dzial=1 or dzial=2)";
-echo("<h3>".$sql."</h3>");
-$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query($sql);
- require("conn.php");
-        echo("<table border=1>");
-        echo("<th>imie</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>data_urodzenia</th>");
-        echo("<th>nazwa_dzial</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-
-echo("<h2>Zadanie 3</h2>");
-$sql = "SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (dzial=1 or dzial=2) and (imie like '%a')";
-echo("<h3>".$sql."</h3>");
-$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query($sql);
- require("conn.php");
-        echo("<table border=1>");
-        echo("<th>imie</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>data_urodzenia</th>");
-        echo("<th>nazwa_dzial</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-
-echo("<h2>Zadanie 4</h2>");
-$sql = "SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (imie like '%a')";
-echo("<h3>".$sql."</h3>");
-$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query($sql);
- require("conn.php");
-        echo("<table border=1>");
-        echo("<th>imie</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>data_urodzenia</th>");
-        echo("<th>nazwa_dzial</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-
-echo("<h2>Zadanie 5</h2>");
-$sql = "SELECT imie, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial = id_org) and (zarobki>50)";
-echo("<h3>".$sql."</h3>");
-$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query($sql);
- require("conn.php");
-        echo("<table border=1>");
-        echo("<th>imie</th>");
-        echo("<th>zarobki</th>");
-        echo("<th>data_urodzenia</th>");
-        echo("<th>nazwa_dzial</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
- 
- echo("<h2>Zadanie 6</h2>");
- $sql = "SELECT sum(zarobki) as suma_zarobki FROM pracownicy, organizacja WHERE (dzial = id_org) and (zarobki>50)";
-echo("<h3>".$sql."</h3>");
-$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query($sql);
- require("conn.php");
-        echo("<table border=1>");
-     
-        echo("<th>suma_zarobki</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["suma_zarobki"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
- 
- echo("<h2>Zadanie 7</h2>");
- $sql = "SELECT count(imie) as liczba_kobiet FROM pracownicy, organizacja WHERE (dzial = id_org and imie like 'a%')";
-echo("<h3>".$sql."</h3>");
-$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query($sql);
- require("conn.php");
-        echo("<table border=1>");
-     
-        echo("<th>liczba_kobiet</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["liczba_kobiet"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
- 
- echo("<h2>Zadanie 8</h2>");
- $sql = "SELECT avg(zarobki) as srednia FROM pracownicy, organizacja WHERE (dzial = id_org) and (dzial=1 or dzial=2)";
-echo("<h3>".$sql."</h3>");
-$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query($sql);
- require("conn.php");
-        echo("<table border=1>");
-     
-        echo("<th>srednia</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["srednia"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
- 
- echo("<h2>Zadanie 9</h2>");
- $sql = "SELECT max(zarobki) as max FROM pracownicy, organizacja WHERE (dzial = id_org)";
-echo("<h3>".$sql."</h3>");
-$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
- $result=$conn->query($sql);
- require("conn.php");
-        echo("<table border=1>");
-     
-        echo("<th>max</th>");
-            while($row=$result->fetch_assoc()) {
-                echo("<tr>");
-                    echo("<td>".$row["max"]."</td>");
-                echo("</tr>");
-            }
-        echo("</table>");
-
-   
- 
-      
+            
 ?>
-<img class="obraz" src="obraz.png">
+<img class="index" src="index.png">
