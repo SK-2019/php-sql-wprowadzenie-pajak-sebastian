d7ef2754259f11360ea24356f5c5ff374df7969b
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2020-11-19 11:24:11 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,7 +2,7 @@
 
 require("conn.php");
 
-$sql = "INSERT INTO Pracownik (null, imie, dzial, zarobki) 
+$sql = "INSERT INTO pracownicy (null, imie, dzial, zarobki) 
        VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
 
 $conn->query($sql);
