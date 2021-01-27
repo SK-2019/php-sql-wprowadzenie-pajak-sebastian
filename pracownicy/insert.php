d6b3529c7de2e928cf09d3032d307b10a4716169
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2021-01-27 20:24:04 +0100

    Add files via upload

---
+++ pracownicy/insert.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+  <link rel="stylesheet" href="stylee.css">
+  <meta name="viewport" content="width=device-width">
+  <title>Insert</title>
+  <div class="nav">
+</BR>
+<a href="danedobazy.php">DODAJ PRACOWNIKA</a>
+<a href="index.php">STRONA GLOWNA</a>
+</div>
+</head>
+<style>
+        body {
+        background-image:url(https://wallpapercart.com/wp-content/uploads/2020/03/Video-Game-The-Witcher-3-Wild-Hunt-The-Witcher-Geralt-of-Rivia-HD-Wallpaper-Background-Imagess.jpg);
+    background-size:cover;
+    background-repeat:no-repeat;
+    background-position:center;
+    background-attachment: fixed;
+  
+        }
+    </style>
+<?php
+echo("<h1>jestes w insert.php</h1>");
+
+
+
+$servername = "remotemysql.com";
+$username = "F1aJmbwBvG";
+$password = "cmCvZxLITd";
+$dbname = "F1aJmbwBvG";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null,'".$_POST['imie']."','".$_POST['dzial']."' , '".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
+
+echo "<h2>".$sql;
+
+
+
+if ($conn->query($sql) === TRUE) {
+  echo("<h1>    </h1>");
+  echo ("<h1>Nowy pracownik dodany :)</h1>");
+} 
+else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+  echo("<h1>Nie udało się :C</h1>");
+}
+
+
+
+
+$conn->close();
+
+?>
+<?php
+
+$sql = "SELECT * FROM pracownicy, organizacja where (dzial = id_org)";
+$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+  require("conn.php");
+        echo("<table border=1>");
+        echo("<th>Id</th>");
+        echo("<th>Imie</th>");
+        echo("<th>Zarobki</th>");
+        echo("<th>Data urodzenia</th>");
+        
+        
+        
+       
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>");
+                  
+                    echo("</tr>");
+                    
+                    
+            }
+            
+        echo("</table>");
+        ?>
+        
+</body>
+</html>
---
+++ pracownicy/orgPracownicy.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+  <link rel="stylesheet" href="stylee.css">
+  <meta name="viewport" content="width=device-width">
+  <title>Organizacja i pracownicy</title>
+</head>
+
+
+        <style>
+        body {
+        background-image:url(https://get.wallhere.com/photo/1920x1080-px-blood-and-wine-CD-Projekt-RED-The-Witcher-The-Witcher-3-Wild-Hunt-video-games-1520999.jpg);
+    background-size:cover;
+    background-repeat:no-repeat;
+    background-position:center;
+    background-attachment: fixed;
+  
+        }
+    </style>
+        
+
+<?php
+echo("<h1>ORGANIZACJA I PRACOWNICY</h1>");
+?>
+    <div class="nav"> 
+  </br>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-pajak-sebastian"><b>GITHUB</b></a>
+    <a href="index.php"><b>STRONA GŁOWNA</b></a></h4>
+    </br>
+    </br>
+    </br>
+    <a href="orgPracownicy.php">ORGANIZACJA I PRACOWNICY</a> 
+     
+    <a href="agregat.php">FUNKCJE AGREGUJĄCE</a> 
+      
+    <a href="pracownicy.php">PRACOWNICY</a> 
+     
+      <a href="dataiczas.php">DATA I CZAS</a> 
+
+      <a href="formularz.html">FORMULARZ</a>
+      
+      <a href="danedobazy.php">DANE DO BAZY</a>
+      </br>
+   </br>
+</br>
+</div>    
+  
+  <?php
+  
+echo("<h2>Pracownicy z nazwa dzialu</h2>");
+$sql = "SELECT * FROM pracownicy, organizacja where (dzial = id_org)";
+echo("<h3>".$sql."</h3>");
+  $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+  require("conn.php");
+        echo("<table border=1>");
+        echo("<th>imie</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  echo("<h2>Pracownicy tylko z dzialu 1 i 4</h2>");
+$sql = "SELECT * FROM pracownicy, organizacja where (dzial = id_org) and (dzial = 1 or dzial = 4)";
+echo("<h3>".$sql."</h3>");
+  $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+  require("conn.php");
+        echo("<table border=1>");
+        echo("<th>imie</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  echo("<h2>Lista kobiet z nazwami działow</h2>");
+$sql = "SELECT * FROM pracownicy, organizacja where (dzial = id_org) and (imie like 'a%')";
+echo("<h3>".$sql."</h3>");
+  $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+  require("conn.php");
+        echo("<table border=1>");
+        echo("<th>imie</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  echo("<h2>Lista mezczyzn z nazwami dzialow</h2>");
+$sql = "SELECT * FROM pracownicy, organizacja where (dzial = id_org) and (imie not like 'a%')";
+echo("<h3>".$sql."</h3>");
+  $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+  require("conn.php");
+        echo("<table border=1>");
+        echo("<th>imie</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  echo("<h2>Pracownicy posortowani malejaco</h2>");
+$sql = "SELECT * FROM pracownicy, organizacja where (dzial = id_org) order by imie desc";
+echo("<h3>".$sql."</h3>");
+  $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+  require("conn.php");
+        echo("<table border=1>");
+        echo("<th>imie</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  echo("<h2>Pracownicy z dzialu 3 posortowani rosnaco po imieniu</h2>");
+$sql = "SELECT * FROM pracownicy, organizacja where (dzial = id_org) order by imie asc";
+echo("<h3>".$sql."</h3>");
+  $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+  require("conn.php");
+        echo("<table border=1>");
+        echo("<th>imie</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  echo("<h2>Kobiety posortowane rosnaco po imieniu</h2>");
+$sql = "SELECT * FROM pracownicy, organizacja where (dzial = id_org) and (imie like '%a') order by imie asc";
+echo("<h3>".$sql."</h3>");
+  $conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+  require("conn.php");
+        echo("<table border=1>");
+        echo("<th>imie</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  echo("<h2>Kobiety z dzialu 1 i 3 posortowane rosnaco po zarobkach</h2>");
+ $sql = "SELECT * FROM pracownicy, organizacja WHERE (dzial = id_org) and (imie like '%a') and (dzial = 1 or dzial = 3) order by zarobki asc";
+echo("<h3>".$sql."</h3>");
+$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+ require("conn.php");
+        echo("<table border=1>");
+        echo("<th>imie</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  echo("<h2>Mezczyzni posortowani rosnaco: po nazwie dzialu a nastepnie po wysokosci zarobkow</h2>");
+ $sql = "SELECT * FROM pracownicy, organizacja WHERE (dzial = id_org) and (imie not like '%a') order by nazwa_dzial asc, zarobki asc";
+echo("<h3>".$sql."</h3>");
+$conn = new mysqli("remotemysql.com","F1aJmbwBvG","cmCvZxLITd","F1aJmbwBvG");
+ $result=$conn->query($sql);
+ require("conn.php");
+        echo("<table border=1>");
+        echo("<th>imie</th>");
+        echo("<th>zarobki</th>");
+        echo("<th>data_urodzenia</th>");
+        echo("<th>nazwa_dzial</th>");
+            while($row=$result->fetch_assoc()) {
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+  
+  
+  ?>
+  
