d78d6f720cde06c7df7fe6db4c3d5d9fada4c78a
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2021-02-02 09:52:43 +0100

    Update danedobazy.php

--- pracownicy/danedobazy.php
+++ pracownicy/danedobazy.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <title>Sebastian Pajak</title>
-  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="shortcut icon" href="/assets/favicon.ico">
 </head>
 <style>
   body {
