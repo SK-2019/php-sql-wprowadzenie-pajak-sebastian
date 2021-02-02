7e423a9a176ae2bef5c53b44bb76e9afc9e5048e
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2021-02-02 09:48:55 +0100

    Update strona.php

--- pracownicy/strona.php
+++ pracownicy/strona.php
@@ -1,9 +1,10 @@
 <html>
         <head>
         <meta charset="utf-8">
-  <link rel="stylesheet" href="stylee.css">
+  <link rel="stylesheet" href="/assets/stylee.css">
   <meta name="viewport" content="width=device-width">
   <title>Sebastian Pajak</title>
+  <link rel="shortcut icon" href="/favicon.ico">
         </head>
   
 </html> 
