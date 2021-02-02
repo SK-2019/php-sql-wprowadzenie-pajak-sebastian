99e5174e7489980ca4cebdcf694eba97b7c26b5c
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2021-02-02 09:53:16 +0100

    Update delete.php

--- pracownicy/delete.php
+++ pracownicy/delete.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sebastian Pajak</title>
-    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="shortcut icon" href="/assets/favicon.ico">
 
 </head>
 <body>
