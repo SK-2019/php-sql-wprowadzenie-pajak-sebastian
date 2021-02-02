4e8328e2a59da3d40c32a2e6fafc14916081e571
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2021-02-02 09:40:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <link rel="stylesheet" href="stylee.css">
   <meta name="viewport" content="width=device-width">
   <title>Sebastian Pajak</title>
+  <link rel="shortcut icon" href="/assets/favicon.ico">
 </head>
 
 <style>
