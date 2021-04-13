a5571c5ff809dd40ac7f5bc16939a62453552ff9
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2021-04-13 09:33:58 +0200

    Add files via upload

---
+++ pracownicy/flexbox.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    
+    <link rel="stylesheet" href="/assets/styleee2.css" />
+  </head>
+  <body>
+      <h2>1</h2>
+    <div class="container">
+        <div class="nav">
+            zad 1
+        </div>
+    </div>
+    <h2> 2</h2>
+    <div class="container2">
+        <div class="nav">
+            zad 2
+        </div>
+    </div>
+    <h2> 3</h2>
+    <div class="container3">
+        <div class="nav">
+            zad 3
+        </div>
+    </div>
+    <h2> 4</h2>
+    <div class="container4">
+        <div class="nav">
+            zad 4
+        </div>
+    </div>
+    <h2> 5</h2>
+    <div class="container5">
+        <div class="nav">
+            zad 5
+        </div>
+    </div>
+    <h2> 6</h2>
+    <div class="container6">
+        <div class="nav">
+            zad 6
+        </div>
+    </div>
+    <h2> 7</h2>
+    <div class="container7">
+        <div class="nav">
+            zad 7
+        </div>
+    </div>
+    <h2> 8</h2>
+    <div class="container8">
+        <div class="nav">
+            zad 8
+        </div>
+        <div class="nav">
+            zad 8
+        </div>
+    </div>
+    <h2> 9</h2>
+    <div class="container9">
+        <div class="nav">
+            zad 9
+        </div>
+        <div class="nav">
+            zad 9
+        </div>
+    </div>
+    <h2> 10</h2>
+    <div class="container10">
+        <div class="nav">
+            zad 10
+        </div>
+        <div class="nav">
+            zad 10
+        </div>
+    </div>
+    <h2> 11</h2>
+    <div class="container11">
+        <div class="nav">
+            zad 11
+        </div>
+        <div class="nav">
+            zad 11
+        </div>
+    </div>
+    <h2> 12</h2>
+    <div class="container12">
+        <div class="nav">
+            zad 12
+        </div>
+        <div class="nav">
+            zad 12
+        </div>
+    </div>
+    </body>
