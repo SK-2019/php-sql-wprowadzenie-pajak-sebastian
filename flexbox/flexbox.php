e6cf5054a10d742b86038d09e143890cbed0389a
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2021-04-13 10:06:34 +0200

    Add files via upload

---
+++ flexbox/flexbox.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    
+    <link rel="stylesheet" href="/assets/styleee2.css" />
+  </head>
+  <body>
+      <h2>1</h2>
+    <div class="cont">
+        <div class="nav">
+            
+        </div>
+    </div>
+    <h2> 2</h2>
+    <div class="cont2">
+        <div class="nav">
+            
+        </div>
+    </div>
+    <h2> 3</h2>
+    <div class="cont3">
+        <div class="nav">
+            
+        </div>
+    </div>
+    <h2> 4</h2>
+    <div class="cont4">
+        <div class="nav">
+            
+        </div>
+    </div>
+    <h2> 5</h2>
+    <div class="cont5">
+        <div class="nav">
+            
+        </div>
+    </div>
+    <h2> 6</h2>
+    <div class="cont6">
+        <div class="nav">
+            
+        </div>
+    </div>
+    <h2> 7</h2>
+    <div class="cont7">
+        <div class="nav">
+            
+        </div>
+    </div>
+    <h2> 8</h2>
+    <div class="cont8">
+        <div class="nav">
+            
+        </div>
+        <div class="nav">
+            
+        </div>
+    </div>
+    <h2> 9</h2>
+    <div class="cont9">
+        <div class="nav">
+            
+        </div>
+        <div class="nav">
+            
+        </div>
+    </div>
+    <h2> 10</h2>
+    <div class="cont10">
+        <div class="nav">
+            
+        </div>
+        <div class="nav">
+            
+        </div>
+    </div>
+    <h2> 11</h2>
+    <div class="cont11">
+        <div class="nav">
+            
+        </div>
+        <div class="nav">
+            
+        </div>
+    </div>
+    <h2> 12</h2>
+    <div class="cont12">
+        <div class="nav">
+            
+        </div>
+        <div class="nav">
+            
+        </div>
+    </div>
+    </body>
