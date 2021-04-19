70522dfbd4796f46b77960a2a2ae727910e014d1
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2021-04-19 21:57:13 +0200

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,10 +1,18 @@
-
 <?php
-
+$hostname = $_SERVER['HTTP_HOST'];
+if ($hostname == 'localhost') {
      require_once ("config.php");
+ }
 
+$servername = $_SERVER['servername'];
+$username = $_SERVER['username'];
+$password = $_SERVER['password'];
+$dbname = $_SERVER['dbname']; 
+
+// Create connection (łącze się z moją bazą danych)
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection (sprawdzam połączenie z bazą danych)
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
