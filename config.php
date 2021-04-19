4e2fcbf60f943085df8bdcf68e3722b4d383da34
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2021-04-19 21:56:44 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
 
 
-$servername = "remotemysql.com";
-$username = "F1aJmbwBvG";
-$password = "cmCvZxLITd";
-$dbname = "F1aJmbwBvG"; 
+//$servername = "remotemysql.com";
+//$username = "F1aJmbwBvG";
+//$password = "cmCvZxLITd";
+//$dbname = "F1aJmbwBvG"; 
 
-
+$_SERVER['servername'] = "remotemysql.com";
+$_SERVER['username'] = "F1aJmbwBvG";
+$_SERVER['password'] = "cmCvZxLITd";
+$_SERVER['dbname'] = "F1aJmbwBvG";
 
 ?>
