61d2e7cc02913a5634708c8702dde5a74ad4970c
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2021-05-13 10:38:21 +0200

    Add files via upload

---
+++ cwiczenia/galeria.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+      rel="stylesheet"
+    />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Antonio:wght@100;200;300&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;1,300;1,400&display=swap"
+      rel="stylesheet"
+    />
+    <title>07-Obrazki</title>
+    <link rel="stylesheet" href="/assets/stylegaleria.css" />
+  </head>
+  <body>
+    <h1>
+      <i class="fas fa-biking"></i>
+      08 - Galeria
+    </h1>
+    <h2>Galeria:</h2>
+
+    <div class="gallery row">
+      <div class="image foto01">1ghjkghjkghjkghjkghjkjhk</div>
+      <div class="image foto02">2</div>
+      <div class="image foto03">3</div>
+    </div>
+
+    <div class="gallery row">
+      <a href="#" class="image foto01">
+        <span class="foto01_title">kurs jazdy moplikiem</span>
+        <span class="foto01_price">cena: 299 zł</span>
+      </a>
+      <a href="#" class="image foto02">
+        <span class="foto01_title">kurs jazdy moplikiem</span>
+        <span class="foto01_price">cena: 299 zł</span>
+      </a>
+      <a href="#" class="image foto03"
+        ><span class="foto01_title">kurs nurkowania</span>
+        <span class="foto01_price">cena: 1299 zł</span></a
+      >
+    </div>
+    <div class="gallery row">
+      <div class="image foto01">1</div>
+      <div class="image foto02">2</div>
+      <div class="image foto03">3</div>
+    </div>
+  </body>
+</html>
