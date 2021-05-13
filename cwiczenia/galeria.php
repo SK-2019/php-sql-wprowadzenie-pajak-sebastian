ccea1830c3ceda50e7e7d1ea962d6365b8c93a77
pajak-sebastian <73524386+pajak-sebastian@users.noreply.github.com>
2021-05-13 10:46:46 +0200

    Add files via upload

--- cwiczenia/galeria.php
+++ cwiczenia/galeria.php
@@ -19,7 +19,7 @@
       href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;1,300;1,400&display=swap"
       rel="stylesheet"
     />
-    <title>07-Obrazki</title>
+    <title>Sebastian Pająk</title>
     <link rel="stylesheet" href="/assets/stylegaleria.css" />
   </head>
   <body>
@@ -30,22 +30,22 @@
     <h2>Galeria:</h2>
 
     <div class="gallery row">
-      <div class="image foto01">1ghjkghjkghjkghjkghjkjhk</div>
+      <div class="image foto01">1</div>
       <div class="image foto02">2</div>
       <div class="image foto03">3</div>
     </div>
 
     <div class="gallery row">
       <a href="#" class="image foto01">
-        <span class="foto01_title">kurs jazdy moplikiem</span>
+        <span class="foto01_title">RockstarGames</span>
         <span class="foto01_price">cena: 299 zł</span>
       </a>
       <a href="#" class="image foto02">
-        <span class="foto01_title">kurs jazdy moplikiem</span>
+        <span class="foto01_title">TheLastOfUs</span>
         <span class="foto01_price">cena: 299 zł</span>
       </a>
       <a href="#" class="image foto03"
-        ><span class="foto01_title">kurs nurkowania</span>
+        ><span class="foto01_title">Gwint</span>
         <span class="foto01_price">cena: 1299 zł</span></a
       >
     </div>
